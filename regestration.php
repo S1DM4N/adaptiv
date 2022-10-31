d8b0e714d066a1cd4c4a9350a59d3db934211fe6
S1DM4N <roma.zavvy2016@gmail.com>
2022-10-31 17:07:03 +0300

    start

---
+++ regestration.php
@@ -0,0 +1,24 @@
+<title>Регистрация</title>
+<link rel="stylesheet" href="css/style.css">
+<body>
+    <div class="wrapper">
+        <main class="reg">
+            <form class="enter" action="#" method="post">
+                <h1>Форма регистрации</h1>
+                <input class="form" type="text" name="login" id="login" placeholder="Логин"> </br></br>
+                <input class="form" type="text" name="name" id="name" placeholder="Имя"> </br></br>
+                <input class="form" type="text" name="second_name" id="second_name" placeholder="Фамилия"> </br></br>
+                <input class="form" type="text" name="phone" id="phone" placeholder="Телефон"> </br></br>
+                <input type="password" name="password" id="password" placeholder="Пароль"> </br></br>
+                <select>
+                    <option>Выбор спортивной секции</option>
+                    <option>Бассейн</option>
+                    <option>Спорт зал</option>
+                </select></br></br>
+                <button type="submit">Отправить</button>
+                </br></br></br></br>
+                <a type="link" class="back" href="index.php">Назад</a>
+            </form>
+        </main>
+    </div>
+</body>
